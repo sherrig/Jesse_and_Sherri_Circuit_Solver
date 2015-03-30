7e480b1d5c2ca5728a5df7ec23bf622fbff071a5
Sherri <sagreen@mit.edu>
2015-03-30 12:19:37 -0400

    Added code to account for voltage drops over devices -- not sure if it works. Still need to add code for Junction rule

--- CircuitSolver.py
+++ CircuitSolver.py
@@ -24,10 +24,37 @@ def solve(in_put):
     b.append(0)
 
     for i in range(len(in_put)):   #length of input should be number of devices
-                                    #assumption that input looks like (battery or resistor, positive terminal, negative terminal, value)
+        new_row = []                #assumption that input looks like (battery or resistor, positive terminal, negative terminal, value)
+        
+                                
         if in_put[i][0] == battery:   #if device is a battery (can change exact way of knowing if it's a battery)
-            new_row = []
-            new_row.insert(i, 1)
+            for i in range(2*len(in_put)-2):   ##put in all zeroes except 2: for the two voltages
+                new_row.append(0)
+                return new_row
+            if i == 0:
+                new_row.insert(i, 1)                #a 1 for the voltage at the positive terminal
+                new_row.insert(len(in_put), -1)     # -1 for the voltage at the negative terminal (if signs are wrong, will be corrected in voltage value)
+            else:
+                new_row.insert(i,1)
+                new_row.insert(i+1, -1)
+                
+            A.append(new_row)
+            b.append(in_put[i].voltage)
+
+        else:
+            for i in range(2*len(in_put)-3):        #3 other spots to be filled
+                new_row.append(0)                   
+                return new_row
+
+            new_row.insert(i-1, 1)
+            new_row.insert(i, -1)
+
+            if in_put[i].direction == True:         #Give resistors attribute to show direction. True = + to -; false = - to +
+                new_row.insert(len(in_put)+i, -1 * in_put[i].resistance)
+            else:
+                new_row.insert(len(in_put)+i, in_put[i].resistance)
+
+            
             
                         
         
