6eacf7f40b03e9b69cab14672f57204fe6159533
Sherri <sagreen@mit.edu>
2015-03-30 20:51:08 -0400

    function changed after talking with Jesse. Only for series circuit.

--- CircuitSolver.py
+++ CircuitSolver.py
@@ -10,50 +10,39 @@
 
 #Set up matrix equation and solve
 
-def solve(in_put):
+def solve(component):
 
     A = np.array([])  #create empty set -- will be matrix with all equations
     b = np.array([])  #create empty set -- will be matrix in eqn Ax=b
 
-    row1 = [1]                          #make row 1 of A say that Vgrd = 0
-    for i in range(2*len(in_put)-1):   #add 1 less than 2*number of devices of zeroes to the first row
-        row1.append(0)
-        return row1
+    for i in range(len(component)):
+        for j in range(len(component)+1):
+            A[i][j] = 0                     #fill with zeroes: need number of rows to be number of components (nodes) plus current (in series circuit, current is all the same)
+            b[j] = 0
+            return A
 
-    A.append(row1)
-    b.append(0)
+    A[0][0] = 0                          #make row 1 of A say that Vgrd = 0
+    
 
-    for i in range(len(in_put)):   #length of input should be number of devices
-        new_row = []                #assumption that input looks like (battery or resistor, positive terminal, negative terminal, value)
-        
+    for i in range(len(component)):   #length of input should be number of devices
+         
                                 
-        if in_put[i][0] == battery:   #if device is a battery (can change exact way of knowing if it's a battery)
-            for i in range(2*len(in_put)-2):   ##put in all zeroes except 2: for the two voltages
-                new_row.append(0)
-                return new_row
-            if i == 0:
-                new_row.insert(i, 1)                #a 1 for the voltage at the positive terminal
-                new_row.insert(len(in_put), -1)     # -1 for the voltage at the negative terminal (if signs are wrong, will be corrected in voltage value)
-            else:
-                new_row.insert(i,1)
-                new_row.insert(i+1, -1)
-                
-            A.append(new_row)
-            b.append(in_put[i].voltage)
+        if component[i].type == "b":   #if device is a battery 
+
+            A[i][i] = -1                #negative terminal voltage should be -1 as coeff
+            A [i][i+1] = 1              #positive terminal voltage should be 1 as coeff
+            
+            b[i] = component[i].voltage
 
         else:
-            for i in range(2*len(in_put)-3):        #3 other spots to be filled
-                new_row.append(0)                   
-                return new_row
 
-            new_row.insert(i-1, 1)
-            new_row.insert(i, -1)
+            A[i][i] = -1
+            A[i][i+1] = 1
+            A[i][len(component)] = -1*component[i].resistance
 
-            if in_put[i].direction == True:         #Give resistors attribute to show direction. True = + to -; false = - to +
-                new_row.insert(len(in_put)+i, -1 * in_put[i].resistance)
-            else:
-                new_row.insert(len(in_put)+i, in_put[i].resistance)
 
+    return A
+    return b
             
             
                         
